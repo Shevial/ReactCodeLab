5ca7d0db4a8d745380aef297a6588fc039ead7e2
Sara HL <sarahevialopez@gmail.com>
2025-02-12 18:21:31 +0100

    Data access

--- ReactProject/src/components/MedicineCard.jsx
+++ ReactProject/src/components/MedicineCard.jsx
@@ -1,18 +1,38 @@
 import { useEffect } from "react"
+import { useState } from "react"
+import './MedicineCard.css'
 
-    function MedicineCard() {
-        useEffect(() => {
-            fetch("http://localhost:8080/api/medicines/view")
-            .then((response) => response.json())
-            .then((data) => console.log(data))
-            .catch(error => console.error("Error:", error));
-        }, [])
+function MedicineCard() {
+
+    const [medicines, setMedicines] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:8080/api/medicines/view/1")
+        .then((response) => response.json())
+        .then((data) => setMedicines(data))
+    }, [])
     
   return (
-    <li className="Medicine-card">
-      <h2 className="medicine-name"></h2>
-    </li>
-  )
+    <ul className="medicine-list">
+      {medicines.map(medicine => (
+        <li key={medicine.id} className="medicine-card">
+          <h2 className="medicine-name">{medicine.name}</h2>
+          <div className="medicine-info">
+          </div>
+          <div className="dosage-info">
+
+            <p>Dosis mínima: {medicine.dosage?.minimum_factor || 'N/A'}</p>
+            <p>Dosis máxima: {medicine.dosage?.maximum_factor || 'N/A'}</p>
+            <p>Frecuencia de dosificación: {medicine.dosage?.dosage_frequency || 'N/A'}</p>
+            <p>Dosis máxima diaria: {medicine.dosage?.max_daily_dose || 'N/A'}</p>
+            <p>Peso medio para el calculo: {medicine.dosage?.avg_weight || 'N/A'}</p>
+
+          </div>
+          <p className="details">{medicine.details}</p>
+        </li>
+      ))}
+    </ul>
+)
 }
 export default MedicineCard
 
--- ReactProject/src/components/MedicineList.jsx
+++ ReactProject/src/components/MedicineList.jsx
@@ -1,9 +1,28 @@
-import MedicineCardo from "./MedicineCardo"
+import { use } from "react"
+import MedicineCard from "./MedicineCard"
 
 function MedicineList() {
+
+useEffect(() => {
+  getMedicines(10);
+}, [])
+
+  const fetchMedicine = async (index) => {
+    const response = await fetch(`http://localhost:8080/api/medicines/view/${index}`)
+    const data = await response.json()
+    return data;
+  }
+  const getMedicines = async (quantity) => {
+    const mdcnArr = [];
+
+    for (let i = 1; i <= quantity; i++) {
+      const medicine = await fetchMedicine(i);
+      mdcnArr.push(medicine);
+    }
+  }
   return (
     <ul className="medicine-list">
-      <MedicineCardo></MedicineCardo>
+      <MedicineCard></MedicineCard>
     </ul>
   )
 }
