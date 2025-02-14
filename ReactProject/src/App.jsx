19377bb03a34661c7da9c02faf3ffe9626c32629
Sara HL <sarahevialopez@gmail.com>
2025-02-14 13:54:17 +0100

    Show info

--- ReactProject/src/App.jsx
+++ ReactProject/src/App.jsx
@@ -2,8 +2,16 @@ import './App.css'
 import MedicineList from './components/MedicineList'
 
 function App() {
+  const [selectedMedicine, setSelectedMedicine] = useState()
   return (
     <>
+    {selectedMedicine && (
+      <div>
+        <h2>Selected medication</h2>
+
+      </div>
+
+    )}
     <h2>Medicine List</h2>
     <MedicineList></MedicineList>
     </>
---
+++ ReactProject/src/components/MedicineDetails.jsx
@@ -0,0 +1,13 @@
+
+function MedicineDetails(props) {
+    const{medicine} = props;
+  return (
+    <Section className="selected-medicine">
+        <div className="medicine-container">
+
+        </div>
+    </Section>
+  )
+}
+
+export default MedicineDetails
