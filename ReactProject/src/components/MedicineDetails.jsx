9f950efa7e0f7b554ea40bb7757cd7c8dfb56e13
Sara HL <sarahevialopez@gmail.com>
2025-02-14 14:24:19 +0100

    Showing all onclick

--- ReactProject/src/components/MedicineDetails.jsx
+++ ReactProject/src/components/MedicineDetails.jsx
@@ -3,7 +3,9 @@ function MedicineDetails(props) {
   return (
     <section className="selected-medicine">
         <div className="medicine-container">
-            <h2 className="text">{medicine.name}</h2>
+            <li className="medicine-card">
+                <h2 className="medicine-name">{medicine.name || 'Medicamento desconocido'}</h2>
+                <p className="details">{medicine.details || 'Sin detalles adicionales'}</p>
             <div className="dosage-info">
                 <p>Dosis mínima: {medicine.dosage?.minimum_factor ?? 'N/A'}</p>
                 <p>Dosis máxima: {medicine.dosage?.maximum_factor ?? 'N/A'}</p>
@@ -11,6 +13,8 @@ function MedicineDetails(props) {
                 <p>Máximo diario: {medicine.dosage?.max_daily_dose ?? 'N/A'}</p>
                 <p>Peso promedio: {medicine.dosage?.avg_weight ?? 'N/A'}</p>
             </div>
+            </li>
+
         </div>
     </section>
   )
